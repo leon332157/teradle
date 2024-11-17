962d17f20894de1f753bb2ec665d45cb04168b64
leon332157 <leon332157@gmail.com>
2024-11-17 17:14:52 -0500

    Squashed commit of the following:
    commit 18120fb7047476854fea7d2e349c7a85d4bb4850
    Merge: ad5ee0d 7eeeb72
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 12:45:10 2024 -0400
    
        Merge branch 'main' into leon-ingame-page
    
    commit ad5ee0d1f62169a31c20d393842cfbaffc53441d
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 11:47:33 2024 -0400
    
        fix null check
    
    commit 6bd14cd60e588970ff4fbc14d3e8347947c6f42f
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 00:19:30 2024 -0400
    
        do my job
    
    commit b91218fa5c348fab00e62ff543fd17c3339982e4
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 00:19:23 2024 -0400
    
        implement basic express server
    
    commit beac39236c10770ca8d3e5f67546d89e4747ba79
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 00:19:16 2024 -0400
    
        implement watch and build
    
    commit ce11cee42bce7bb0f6a7302da03fff83ada1288a
    Author: leon332157 <leon332157@gmail.com>
    Date:   Fri Nov 1 00:18:59 2024 -0400
    
        update deps

--- gulpfile.js
+++ gulpfile.js
@@ -82,6 +82,11 @@ const watchAndServe = series(parallel(copyHtml, copyAssets), series(compileTSFro
 exports.browsersyncServe = browsersyncServe;
 exports.watch = watch;
 exports.watchAndServe = watchAndServe;
+<<<<<<< HEAD
 const frontend = series(copyHtml, copyAssets,copyCSS,copyJS, compileTSFrontend);
+=======
+
+const frontend = series(copyHtml, copyAssets, compileTSFrontend);
+>>>>>>> leon-ingame-page
 const backend = series(compileTSBackend);
 exports.default = series(frontend, backend);
