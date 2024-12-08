3a85041e150e84b6b8b17e707517ef7415bebec3
leon332157 <leon332157@gmail.com>
2024-12-07 23:13:05 -0500

    #13: Add routes for front-end

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -7,15 +7,43 @@ const port = 8000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(join(dirname(__filename), '..')));
+
+/* router for the API */
+const apiRouter = new ApiRouter();
+app.use(apiRouter.getRouter());
+
+const frontendRouter = express.Router();
+frontendRouter.get('/', (req, res) => {
+  res.sendFile(join(dirname(__filename), '..', 'quiz-list.html'));
+});
+
+frontendRouter.get('/create-quiz', (req, res) => {
+  res.sendFile(join(dirname(__filename), '..', 'create-quiz.html'));
+});
+
+frontendRouter.get('/join-quiz', (req, res) => {
+  res.sendFile(join(dirname(__filename), '..', 'join-pin.html'));
+});
+
+frontendRouter.get('/host-quiz', (req, res) => {
+  res.sendFile(join(dirname(__filename), '..', 'quiz-session.html'));
+});
+
+frontendRouter.get('/in-game/instructor/:sessionid/:questionNum', (req, res) => {
+  
+});
+
+frontendRouter.get('/in-game/student/:sessionid/:questionNum', (req, res) => {
+
+});
+
+frontendRouter.get('/leaderboard/instructor/:sessionid', (req, res) => {
+});
+app.use(frontendRouter);
+
 app.on('error', (err) => {
   console.error(err);
-}
-);
-
-
-const apiRouter = new ApiRouter();
-
-app.use(apiRouter.getRouter());
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
