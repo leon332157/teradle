57fa12163a10d42016171f696b64e93e9069f3ce
Selena Lo <tsaihsilo@gmail.com>
2024-12-08 17:20:28 -0500

    Use js instead of ts for frontend, use iife with defer property, load leaderboard

--- src/frontend/leaderboard-scoring.js
+++ src/frontend/leaderboard-scoring.js
@@ -1,16 +1,44 @@
-"use strict";
-document.addEventListener("DOMContentLoaded", () => {
-  console.log('sorting function');
-  const leaderboard = document.querySelector('.display-top-players');
-  if (leaderboard) {
-    const players = Array.from(leaderboard.getElementsByClassName('player'));
-    players.sort((a, b) => {
-      const scoreA = parseInt(a.querySelector('.player-score')?.textContent || '0', 10);
-      const scoreB = parseInt(b.querySelector('.player-score')?.textContent || '0', 10);
-      return scoreB - scoreA;
-    });
-    console.log('sorted players', players);
-    leaderboard.innerHTML = '';
-    players.forEach(player => leaderboard.appendChild(player));
+(
+  async () => {
+    try {
+      const leaderboard = await fetch('http://localhost:8000/api/leaderboard-scoring/getLeaderboard');
+
+      if (!leaderboard.ok) {
+        throw new Error(`Failed to fetch leaderboard: ${leaderboard.status}`);
+      }
+      const json = await leaderboard.json();
+
+      const leaderboardArray = json.leaderboard;
+
+      const leaderboardElement = document.querySelector('.display-top-players');
+
+      if (leaderboardElement === null) {
+        return;
+      }
+
+      leaderboardArray.forEach((participant) => {
+        leaderboardElement.appendChild(getParticipantElement(participant));
+      });
+    }
+    catch (error) {
+      console.error(`Error loading leaderboard: ${error.message}`);
+    }
   }
-});
+)();
+
+const getParticipantElement = (participant) => {
+  const div = document.createElement("div");
+  div.className = "player";
+
+  const player_name = document.createElement("span");
+  player_name.className = "player-name";
+  player_name.innerHTML = participant.name;
+
+  const player_score = document.createElement("span");
+  player_score.className = "player-score";
+  player_score.innerHTML = participant.score.toString();
+
+  div.appendChild(player_name);
+  div.appendChild(player_score);
+  return div;
+}
