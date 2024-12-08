aa84b28b8d8126ec85b8dc9572d90e819de11d15
Selena Lo <tsaihsilo@gmail.com>
2024-12-08 14:20:37 -0500

    Update leaderboard-scoring api

--- src/backend/api/router.ts
+++ src/backend/api/router.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
-import {getTest} from "./test";
-import {getSingleQuiz} from "./quiz";
+import { getTest } from "./test";
+import { getSingleQuiz } from "./quiz";
+import { getLeaderboard } from "./leaderboard-scoring";
 
 export class ApiRouter {
   private router: express.Router;
@@ -14,6 +15,7 @@ export class ApiRouter {
   private initRoutes() {
     this.router.get('/api/test', getTest);
     this.router.get('/api/quiz/single', getSingleQuiz);
+    this.router.get('/api/leaderboard-scoring', getLeaderboard);
   }
 
   public getRouter() {
